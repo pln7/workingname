5e40fafce876a4a45f6fcc21d67effa2e472a42b
ercana <jolan.cubells@gmail.com>
2014-03-01 22:13:52 +0100

    Gabarits de vacations valides
    Le .js modifié en plus du html ça peut servir^^

--- js/ts_angular.js
+++ js/ts_angular.js
@@ -9,65 +9,91 @@
  * controleur angular du tableau de service
  */
 function tsCtrl($scope) {
-	$scope.agents = [ // liste des agents TODO : récupérer à partir du JSON
-	                 {
-	                	 name : 'pierre', // matricule TODO : récupérer à partir du JSON
-	                	 ts : [
-	                	       {
-	                	    	   matin : 'abs',
-	                	    	   aprem : 'abs',
-	                	       },
-	                	       {
-	                	    	   matin : 'null',
-	                	    	   aprem : 'null',
-	                	       },
-	                	       {
-	                	    	   matin : 'vac',
-	                	    	   aprem : 'abs',
-	                	       },
-	                	       {
-	                	    	   matin : 'vac',
-	                	    	   aprem : 'vac',
-	                	       },
-	                	       {
-	                	    	   matin : 'vac',
-	                	    	   aprem : 'abs',
-	                	       }
-	                	       ]
-	                 },
-	                 
-	                 {
-	                	 name : 'jacques',
-	                	 ts : [
-	                	       {
-	                	    	   matin : 'abs',
-	                	    	   aprem : 'abs',
-	                	       },
-	                	       {
-	                	    	   matin : 'vac',
-	                	    	   aprem : 'abs',
-	                	       },
-	                	       {
-	                	    	   matin : 'vac',
-	                	    	   aprem : 'abs',
-	                	       },
-	                	       {
-	                	    	   matin : 'vac',
-	                	    	   aprem : 'vac',
-	                	       },
-	                	       {
-	                	    	   matin : 'vac',
-	                	    	   aprem : 'abs',
-	                	       }
-	                	       ]
-	                 }
-	                 ];	
+
+
 	
+
+
+
+
+	$scope.agents = [ // liste des agents TODO : récupérer à partir du JSON
+	                  {
+	                	  name : 'pierre', // matricule TODO : récupérer à partir du JSON
+	                	  ts : [
+	                	        {
+	                	        	matin : 'abs',
+	                	        	aprem : 'abs',
+	                	        },
+	                	        {
+	                	        	matin : 'null',
+	                	        	aprem : 'null',
+	                	        },
+	                	        {
+	                	        	matin : 'vac',
+	                	        	aprem : 'abs',
+	                	        },
+	                	        {
+	                	        	matin : 'vac',
+	                	        	aprem : 'vac',
+	                	        },
+	                	        {
+	                	        	matin : 'vac',
+	                	        	aprem : 'abs',
+	                	        }
+	                	        ]
+	                  },
+
+	                  {
+	                	  name : 'jaques',
+	                	  ts : [
+	                	        {
+	                	        	matin : 'abs',
+	                	        	aprem : 'abs',
+	                	        },
+	                	        {
+	                	        	matin : 'vac',
+	                	        	aprem : 'abs',
+	                	        },
+	                	        {
+	                	        	matin : 'vac',
+	                	        	aprem : 'abs',
+	                	        },
+	                	        {
+	                	        	matin : 'vac',
+	                	        	aprem : 'vac',
+	                	        },
+	                	        {
+	                	        	matin : 'vac',
+	                	        	aprem : 'abs'
+	                	        }
+	                	        ]
+	                  }
+	                  ]
+
 	$scope.click = function(toPrint) {
-		window.open('form_ts.html','_blank');
-		return false;
-		// alert(toPrint);
-	}
-}
+		$scope.getGabarits();
+	};
+	
+	$scope.getGabarits = function() {
+		//récupération des gabarits de vacations du serveur
+		var json = listeGabaritsVacations();
+		var objet = json2Object(json);
+		var listegabarits = objet["gabarits"];
+
+		var temp;
+		var tab = new Array();
+		for (i in listegabarits) {
+			temp = listegabarits[i];
+			tab.push(temp["gabcourt"]);
+		}
+		return tab;
+	};
+
+	$scope.listeGabVacation = $scope.getGabarits();
+
+
+
+};
+
 
 
