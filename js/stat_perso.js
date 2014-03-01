c1f1b8355f6c512a7b08a7fe145bc48b0d33d9a7
Mcherioux <cherioux.morgan@gmail.com>
2014-03-02 00:50:44 +0100

    maj de stat_perso
    HTML et angularjs pour l'étape 4 ( Etape 3 a compléter par Tanguy d'ici
    peu ) + maj du rendu global
    Reste à cacher les tableaux non concernés par le choix lors de l'étape 1
    ( probablement besoin d'aide, suite lundi )

---
+++ js/stat_perso.js
@@ -0,0 +1,306 @@
+function GabCtrl($scope){
+
+$scope.list_gab_abs = [
+   {
+    name : 'AHAD : Aut. d\'abs. pour siéger en commission d\'adoption',
+    abrev : 'AHAD',
+    selected : false
+   },
+   {
+    name : 'AHAL : Aut. d\'abs. pour allaitement',
+    abrev : 'AHAL',
+    selected : false
+   },
+   {
+    name : 'AHDS : Aut. d\'abs. pour don du sang et de ses composantes',
+    abrev : 'AHDS',
+    selected : false
+   },
+   {
+    name : 'AHFE : Aut. d\'abs. facilité horaire accordée aux femmes enceintes',
+    abrev : 'AHFE',
+    selected : false
+   },
+   {
+    name : 'AHME : Aut. d\'abs. pour examen médicaux spéciaux',
+    abrev : 'AHME',
+    selected : false
+   },
+   {
+    name : 'AHPE : Aut. d\'abs. aux parents d\'élèves pour réunions de comités d\'établiss. ou de pare',
+    abrev : 'AHPE',
+    selected : false
+   },
+   {
+    name : 'AHPO : Aut. d\'abs. pour un mandat local ou territorial',
+    abrev : 'AHPE',
+    selected : false
+   },
+   {
+    name : 'AHSY : Facilité horaire pour l\'information syndicale',
+    abrev : 'AHSY',
+    selected : false
+   },
+   {
+    name : 'ATAS : Autorisation temporaire d\'activités sociales',
+    abrev : 'AHSY',
+    selected : false
+   },
+   {
+    name : 'AUAC : Aut. d\'abs. prépa à l\'accouchement sans douleur',
+    abrev : 'AHSY',
+    selected : false
+   },
+   {
+    name : 'AUAD : Aut. d\'abs. pour siéger en commission d\'adoption',
+    abrev : 'AUAD',
+    selected : false
+   },
+   {
+    name : 'AUCO : Aut. d\'abs. pour un congrès politique',
+    abrev : 'AUCO',
+    selected : false
+   },
+   {
+    name : 'AUEC : Congé pour préparation et participation aux examens et concours',
+    abrev : 'AUEC',
+    selected : false
+   },
+   {
+    name : 'AUEM : Aut. d\'abs. pour garder un enfant malade',
+    abrev : 'AUEM',
+    selected : false
+   },
+   {
+    name : 'AUEV : Aut. d\'abs. pour évènement de famille',
+    abrev : 'AUEV',
+    selected : false
+   },
+   {
+    name : 'AUJU : Aut. d\'abs. pour un jury d\'assise',
+    abrev : 'AUJU',
+    selected : false
+   },
+   {
+    name : 'AUMA : Aut. d\'abs. à l\'occasion de la maternité',
+    abrev : 'AUMA',
+    selected : false
+   },
+   {
+    name : 'AUME : Aut. d\'abs. pour les examens médicaux',
+    abrev : 'AUME',
+    selected : false
+   },
+   {
+    name : 'AUMI : Aut. d\'abs. pour cohabitant avec une personne contagieuse',
+    abrev : 'AUMI',
+    selected : false
+   },
+   {
+    name : 'AUPO : Aut. d\'abs. pour un mandat local ou territorial',
+    abrev : 'AUPO',
+    selected : false
+   },    
+   {
+    name : 'AUPR : Aut. d\'abs. à l\'occasion des élections prud\'homales',
+    abrev : 'AUPR',
+    selected : false
+   },    
+   {
+    name : 'AUPT : Aut. d\'abs. pour candidature à un mandat parlementaire ou territorial',
+    abrev : 'AUPT',
+    selected : false
+   },    
+   {
+    name : 'AURE : Aut. d\'abs. à l\'occasion des fêtes religieuses',
+    abrev : 'AURE',
+    selected : false
+   },    
+   {
+    name : 'AUSP : Aut. d\'abs. pour les sapeurs-pompiers volontaires',
+    abrev : 'AUSP',
+    selected : false
+   },    
+   {
+    name : 'AUSY : Aut. d\'abs. pour, à titre syndical, élection ou participation à diff. organismes',
+    abrev : 'AUSY',
+    selected : false
+   },    
+   {
+    name : 'AUXM : Autorisation d\'absence exceptionnelle ministérielle',
+    abrev : 'AUXM',
+    selected : false
+   },    
+   {
+    name : 'CAAD : Congés annuels administratifs',
+    abrev : 'CAAD',
+    selected : false
+   },    
+   {
+    name : 'CAHS : Jour de fractionnement pour congés annuels hors saison',
+    abrev : 'CAHS',
+    selected : false
+   },    
+   {
+    name : 'CANN : Congés annuels',
+    abrev : 'CANN',
+    selected : false
+   },    
+   {
+    name : 'CAOM : Congés bonifiés pour l\'Outre-mer',
+    abrev : 'CAOM',
+    selected : false
+   },    
+   {
+    name : 'CCET : Congé Compte Epargne Temps',
+    abrev : 'CCET',
+    selected : false
+   },    
+   {
+    name : 'CMAC : Congés de maladie pour accident de service',
+    abrev : 'CMAC',
+    selected : false
+   },    
+   {
+    name : 'CMFP : Congés de maladie ordinaire',
+    abrev : 'CMFP',
+    selected : false
+   },    
+   {
+    name : 'CMLD : Congés de maladie de longue durée',
+    abrev : 'CMLD',
+    selected : false
+   },    
+   {
+    name : 'CMLM : Congés de maladie de longue maladie',
+    abrev : 'CMLM',
+    selected : false
+   },    
+   {
+    name : 'CSAN : Congé pour formation à l\'animation',
+    abrev : 'CSAN',
+    selected : false
+   },    
+   {
+    name : 'CSDO : Congé d\'adoption',
+    abrev : 'CSDO',
+    selected : false
+   },    
+   {
+    name : 'CSDS : Congé supplémentaire d\'adoption',
+    abrev : 'CSDS',
+    selected : false
+   },    
+   {
+    name : 'CSFP : Congé formation professionnelle',
+    abrev : 'CSFP',
+    selected : false
+   },    
+   {
+    name : 'CSMA : Congé de maternité',
+    abrev : 'CSMA',
+    selected : false
+   },    
+   {
+    name : 'CSMI : Congé pour période d\'instruction militaire ou activité dans la réserve',
+    abrev : 'CSMI',
+    selected : false
+   },    
+   {
+    name : 'CSNA : Congé supplémentaire de naissance',
+    abrev : 'CSNA',
+    selected : false
+   },    
+   {
+    name : 'CSPA : Congé parental',
+    abrev : 'CSPA',
+    selected : false
+   },    
+   {
+    name : 'CSPP : Congé pour présence parentale',
+    abrev : 'CSPP',
+    selected : false
+   },    
+   {
+    name : 'CSPR : Congé de paternité',
+    abrev : 'CSPR',
+    selected : false
+   },    
+   {
+    name : 'CSSP : Conditions particulières d\'emploi pour les sportifs de haut niveau',
+    abrev : 'CSSP',
+    selected : false
+   },    
+   {
+    name : 'CSSY : Congé pour formation syndicale',
+    abrev : 'CSSY',
+    selected : false
+   },    
+   {
+    name : 'CSVI : Congé pour accompagnement d\'un parent ou cohabitant en fin de vie',
+    abrev : 'CSVI',
+    selected : false
+   },    
+   {
+    name : 'GREV : Jour de Grève',
+    abrev : 'GREV',
+    selected : false
+   },    
+   {
+    name : 'KHMI : Compensation horaire Missions ou autres',
+    abrev : 'KHMI',
+    selected : false
+   },      
+   {
+    name : 'KJMI : Compensation Missions ou autres',
+    abrev : 'KJMI',
+    selected : false
+   },    
+   {
+    name : 'REAG : Récupération "après-midi DIR/AG"',
+    abrev : 'REAG',
+    selected : false
+   }, 
+   {
+    name : 'REBL : Récupération bilan ou crédit/débit',
+    abrev : 'REBL',
+    selected : false
+   }, 
+   {
+    name : 'RETP : Récupération temps partiel Posté mensuel/annuel',
+    abrev : 'RETP',
+    selected : false
+   }, 
+   {
+    name : 'RHAG : Récupération horaire "après-midi DIR/AG"',
+    abrev : 'RHAG',
+    selected : false
+   }, 
+   {
+    name : 'RHBL : Récupération horaire bilan ou crédit/débit',
+    abrev : 'RHBL',
+    selected : false
+   }, 
+   {
+    name : 'SJMT : Jour mi-temps thérapeutique',
+    abrev : 'SJMT',
+    selected : false
+   }, 
+   {
+    name : 'SJRL : JRTT libre',
+    abrev : 'SJRL',
+    selected : false
+   }, 
+   {
+    name : 'SJTP : Jour Temps Partiel',
+    abrev : 'SJTP',
+    selected : false
+   }, 
+   {
+    name : 'TJMI : Trajet mission',
+    abrev : 'TJMI',
+    selected : false
+   }
+]
+
+}
